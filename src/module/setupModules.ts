ed05399612f9d9974ffdcb64b37be7e92f91b31f
Tim Posney <tposney@gmail.com>
2021-11-07 16:17:16 +1100

    package

--- src/module/setupModules.ts
+++ src/module/setupModules.ts
@@ -88,7 +88,19 @@ export function checkCubInstalled() {
 Hooks.once('libChangelogsReady', function() {
   //@ts-ignore
   libChangelogs.register("midi-qol",`
-  0.8.79
+  0.8.80
+  * "full damage on save" to configure save damage for spells (like no damage on save it is always checked) - full damage on save would be used for spells that always do their damage but have contingent effects, like poisoned on a failed save.
+  * Added roll other damage for spells with the same settings as roll other damage for rwak/mwak.
+  * Fix for TrapWorkflow not targeting via templates correctly.
+  * Corrected tooltip for saving throw details when using better rolls (was always displaying 1d20).
+  * Correction to Divine Smite sample item which was incorrectly adding the bonus damage for improved divine smite.
+  * Fix for better rolls AoE spells failing if the template was placed before the damage roll completed (i.e. when dice so nice enabled).
+  * Fix for midi-qol not picking up the damage types for versatile damage rolls.
+  * Tidied up Readme.md
+
+  * Discovered, but have not fixed that if a) using better rolls, b) not using merge card and c) using dice so nice then save results won't be displayed to the chat. So if using better rolls you should enable merge card.
+
+0.8.79
   * fix for overtime effects duplicating convenient effects when the name of the effect being checked matches a convenient effect.
   * fix for TrapWorkflow not displaying the damage type list in the roll flavor.
   * Add new config option to bypass the spell cast dialog, casting at default level and placing templates. Pressing both Advantage+Disadvantage keys will force display of the casting dialog. If you don't have a spell slot of the level of the spell the dialog will be displayed so you can choose another slot. 
